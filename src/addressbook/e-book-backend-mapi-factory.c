fac74e7a13ae39b13ced7cbf2c120352924452b8
Johnny Jacob <johnnyjacob@gmail.com>
2010-01-18 11:52:59 +0530

    Updating overlooked licence string.

--- src/addressbook/e-book-backend-mapi-factory.c
+++ src/addressbook/e-book-backend-mapi-factory.c
@@ -1,23 +1,23 @@
 /* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
 /*
- *  Authors: 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with the program; if not, see <http://www.gnu.org/licenses/>  
+ *
+ *
+ * Authors:
  *    Srinivasa Ragavan <sragavan@novell.com>
  *
- *  Copyright (C) 1999-2008 Novell, Inc. (www.novell.com)
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of version 2 of the GNU Lesser General Public
- *  License as published by the Free Software Foundation.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Lesser General Public License for more details.
- *
- *  You should have received a copy of the GNU Lesser General Public 
- *  License along with this program; if not, write to: 
- *  Free Software Foundation, 51 Franklin Street, Fifth Floor,
- *  Boston, MA 02110-1301, USA.
+ * Copyright (C) 1999-2008 Novell, Inc. (www.novell.com)
  *
  */
 
